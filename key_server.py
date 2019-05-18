53b74e4a23d33f3563557f11fb95a27024eaccfe
Nazarzastavil <43075855+Nazarzastavil@users.noreply.github.com>
2019-05-18 08:14:14 +0500

    client_server

---
+++ key_server.py
@@ -0,0 +1,41 @@
+import socket
+import sys
+import traceback
+import glob
+from threading import Thread
+
+
+def main():
+    start_server()
+
+
+def start_server():
+    host = socket.gethostname()
+    port = 1337         # arbitrary non-privileged port
+
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
+    print("Socket created")
+
+    try:
+        soc.bind((host, port))
+    except:
+        print("Bind failed. Error : " + str(sys.exc_info()))
+        sys.exit()
+
+    soc.listen(5)       # queue up to 5 requests
+    print("Socket now listening")
+
+    # infinite loop- do not reset for every requests
+    while True:
+        connection, address = soc.accept()
+        ip, port = str(address[0]), str(address[1])
+        print("Connected with " + ip + ":" + port)
+
+        try:
+            Thread(target=client_thread, args=(connection, ip, port)).start()
+        except:
+            print("Thread did not start.")
+            traceback.print_exc()
+
+    soc.close()
